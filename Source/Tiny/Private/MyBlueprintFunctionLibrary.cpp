020b40eda3f3a2ffa3e3479a6c03c546eea53d66
Alberto R <alberto@3in-tech.com>
2023-02-01 05:29:01 +0100

    Sprint 4
    conquer fix, watch values, hardware cursor, web browser, cursor offset, fix: bar, nav mesh, balance

---
+++ Source/Tiny/Private/MyBlueprintFunctionLibrary.cpp
@@ -0,0 +1,13 @@
+
+
+
+#include "MyBlueprintFunctionLibrary.h"
+
+// 	#include "GenericPlatform/ICursor.h"
+// void UMyBlueprintFunctionLibrary::SetTypeShape()
+//{
+//     ICursor* Cursor = FSlateApplication::Get().GetPlatformCursor().Get();
+//     if (Cursor) {
+//         Cursor->SetTypeShape(EMouseCursor::Custom, nullptr);
+//     }
+// }
---
+++ Source/Tiny/Public/MyBlueprintFunctionLibrary.h
@@ -0,0 +1,26 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINY_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
+
+
+// UCLASS()
+// class TINY_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary {
+//     GENERATED_BODY()
+//
+//     UFUNCTION(BlueprintCallable, Category = "Cursor")
+//     void SetTypeShape();
+// };
